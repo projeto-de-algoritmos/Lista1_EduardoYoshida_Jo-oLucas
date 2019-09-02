66f94b384bdaf44dd93525e7f12e8af5fe448e9c
Yoshida-Eduardo <edry97@hotmail.com>
2019-09-01 22:53:37 -0300

    Adds stack implementation, which will be used to implement DFS

---
+++ graph/stack.go
@@ -0,0 +1,30 @@
+package graph
+
+import "sync"
+
+type Stack struct {
+	nodes []Node
+	mutex sync.RWMutex
+}
+
+func (s *Stack)New() *Stack{
+	s.mutex.Lock()
+	s.nodes = []Node{}
+	s.mutex.Unlock()
+
+	return s
+}
+
+func (s *Stack)Add(n Node){
+	s.mutex.Lock()
+	s.nodes = append(s.nodes, n)
+	s.mutex.Unlock()
+}
+
+func (s *Stack)Pop() *Node{
+	s.mutex.Lock()
+	node := s.nodes[len(s.nodes)-1]
+	s.nodes = s.nodes[0:len(s.nodes)-1]
+	s.mutex.Unlock()
+	return &node
+}
